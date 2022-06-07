2d2f99d0a73a545c19e523f7a82ee5f0ab92bb56
06Elija <elija.dirren@gmail.com>
2022-06-07 11:54:11 +0200

    Fixed player scripts, and updated the template

--- Assets/Scripts/Core/Behaviors/PlayerBehavior.cs
+++ Assets/Scripts/Core/Behaviors/PlayerBehavior.cs
@@ -14,16 +14,14 @@ public class PlayerBehavior : AgentBehaviour
     public GameObject GameManager; // Game manager object
     private GameManager gameManager;
     private Zone currentZone; // The current zone the player is in, none, the code transmission zone, or the door zone
-    private bool correctCodeEntered; // Whether the correct code has been entered yet or not
-    private GameObject codeZone; // Zone where the code is shown
-    private GameObject doorZone; // Zone where the code is read
-    private List<int> enteredCode; // the code currently entered by the user
 
     public float linSpeed; // Linear speed to move to the center of a zone
     public float angSpeed; // Angular speed to move to the center of a zone
 
     public float epsilon; // Precision of position checks
     public InputKeyboard inputKeyboard; 
+    private bool atCorrectAngle; // Whether the cellulo is already at the correct angle
+    public float wallRange; // Range at which walls are detected
 
     // Enum which describes in which zone the player is currently in
     private enum Zone
@@ -33,49 +31,13 @@ public class PlayerBehavior : AgentBehaviour
         Door // Door zone
     }
 
-    // Enum to specify what is returned to the cellulos' scripts
-    public enum CodeCheckReturn
-    {
-        Incomplete,
-        Wrong,
-        Correct
-    }
-
     void Start()
     {
-        agent.SetCasualBackdriveAssistEnabled(true);
+        agent.SetCasualBackdriveAssistEnabled(true); // TODO remove this?
         gameManager = GameManager.GetComponent<GameManager>();
 
-    }
+        agent.ActivateDirectionalHapticFeedback(); // TODO test this, should work with walls directly, otherwise activate normal wall response mode
 
-    void OnCollisionEnter(Collision collisionInfo)
-    {
-        if (!gameManager.isWebGame())
-        {
-            if (collisionInfo.collider.tag == "CodeZone" || collisionInfo.collider == doorZone)
-            {
-                // Move the cellulo to the center of the zone
-                agent.isMoved = false;
-                agent.SetCasualBackdriveAssistEnabled(false);
-                //Vector3 position = collisionInfo.collider.transform.position;
-                //agent._celluloRobot.SetGoalPose(position.x, position.z, 0, linSpeed, angSpeed);
-                //StartCoroutine(waitUntilAtSpot(position.x, position.z, 0));
-                if (collisionInfo.collider.tag == "CodeZone")
-                {
-                    currentZone = Zone.Code;
-                }
-                else if (!correctCodeEntered)
-                {
-                    currentZone = Zone.Door;
-                }
-            }
-        }
-    }
-
-    // Coroutine to wait until the cellulo is at a given position and angle
-    private IEnumerator waitUntilAtSpot(float x, float y, float t)
-    {
-        yield return new WaitUntil(() => Math.Abs(agent._celluloRobot.GetX() - x) <= epsilon && Math.Abs(agent._celluloRobot.GetY() - y) <= epsilon && Math.Abs(agent._celluloRobot.GetTheta() - t) <= epsilon);
     }
 
     void FixedUpdate()
@@ -107,6 +69,7 @@ public class PlayerBehavior : AgentBehaviour
     {
         // TODO: show correct orientation, or move to correct orientation
 
+
         for(int i = 0; i < code.Length; ++i)
         {
             agent.SetVisualEffect(VisualEffect.VisualEffectConstAll, Color.black, 0);
@@ -122,16 +85,41 @@ public class PlayerBehavior : AgentBehaviour
         yield return new WaitForSeconds(gameManager.waitTime);
     }
 
+    // Coroutine to wait for a short amount of time
+    private IEnumerator waitForShort()
+    {
+        yield return new WaitForSeconds(0.1f);
+    }
+
+
+    // Goal angle reached
+    public void OnGoalPoseReached()
+    {
+        atCorrectAngle = true;
+    }
+
     // Method to read the code entered on the cellulo. Blinks green if the code is correct and red otherwise
     public bool readCode(int[] correctCode, Color color)
     {
+        // Turn cellulo to correct orientation
+        if(Math.Abs(agent._celluloRobot.GetTheta()) > epsilon)
+        {
+            agent._celluloRobot.SetGoalOrientation(0, 50);
+            while(!atCorrectAngle)
+            {
+                StartCoroutine(waitForShort());
+            }
+            atCorrectAngle = false;
+        }
+
+
         //light up led zero to know orientation of code and color of door
         agent.SetVisualEffect(VisualEffect.VisualEffectConstSingle, color, 0);
         //TODO: play sound
 
         // get the code the player enters on the cellulo leds
-        int length = gameManager.getCodeLength();
-        CodeCheckReturn returnCode = CodeCheckReturn.Incomplete;
+        int length = correctCode.Length;
+        int[] enteredCode = new int[length];
         for(int i = 0; i < length; ++i)
         {
             bool found = false;
@@ -149,24 +137,23 @@ public class PlayerBehavior : AgentBehaviour
                 }
             }
             // add digit to enteredCode and check for correctness
-            returnCode = enterKeyStroke(currentKey, correctCode);
+            enteredCode[i] = currentKey;
             StartCoroutine(waitForRelease(currentKey));
         }
-        if(returnCode == CodeCheckReturn.Correct)
+        bool codeIsCorrect = true;
+        for(int i = 0; i < length; ++i)
         {
-            correctCodeEntered = true;
-            agent.SetVisualEffect(VisualEffect.VisualEffectPulse, Color.green, 0);
-            //TODO: play sound;
+            if(enteredCode[i] != correctCode[i])
+            {
+                codeIsCorrect = false;
+                break;
+            }
         }
-        else
-        {
-            correctCodeEntered = false;
-            agent.SetVisualEffect(VisualEffect.VisualEffectPulse, Color.red, 0);
-            //TODO: play sound;
-        }
+        agent.SetVisualEffect(VisualEffect.VisualEffectPulse, (codeIsCorrect) ? Color.green : Color.red, 0);
+        // TODO: play sound
         StartCoroutine(waitForTime());
         agent.SetVisualEffect(VisualEffect.VisualEffectConstAll, Color.black, 0);
-        return correctCodeEntered;
+        return codeIsCorrect;
     }
 
     // Coroutine which waits until a given key is released
@@ -175,73 +162,68 @@ public class PlayerBehavior : AgentBehaviour
         yield return new WaitUntil(() => agent._celluloRobot.GetTouch(key) == Touch.TouchReleased);
     }
 
-  
-    // Method to check if the entered code of the player is correct, assumes the entered code is already long enough
-    private bool checkCode(int[] code)
-    {
-        for(int i = 0; i < gameManager.getCodeLength(); ++i)
-        {
-            if(enteredCode[i] != code[i])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
- 
-    // Method to reset the codes entered by a given player
-    private void resetEnteredCode()
-    {
-        enteredCode.Clear();
-
-    }
-
-    // Method to register a key stroke in a given code. This returns Correct if the code is correct, Wrong if the code is wrong (as well
-    // as resetting the entered code) and Incomplete if the code has not been fully sent yet
-    private CodeCheckReturn enterKeyStroke(int key, int[] code)
-    {
-        enteredCode.Add(key);
-        if (enteredCode.Count == code.Length)
-        {
-            if (checkCode(code))
-            {
-                resetEnteredCode();
-                return CodeCheckReturn.Correct;
-            }
-            else
-            {
-                resetEnteredCode();
-                return CodeCheckReturn.Wrong;
-            }
-        }
-        else {
-            return CodeCheckReturn.Incomplete;
-        }  
-    }
-
     // MOTION 
 
     public override Steering GetSteering()
     {
         Steering steering = new Steering();
 
-        float horizontal;
-        float vertical;
+        if(gameManager.isWebGame())
+        {
+            float horizontal;
+            float vertical;
 
-        if(inputKeyboard == InputKeyboard.arrows){
-            horizontal=Input.GetAxis("HorizontalArrows");
-            vertical=Input.GetAxis("VerticalArrows");
+            if(inputKeyboard == InputKeyboard.arrows){
+                horizontal=Input.GetAxis("HorizontalArrows");
+                vertical=Input.GetAxis("VerticalArrows");
+            }
+            else{
+                horizontal=Input.GetAxis("HorizontalWASD");
+                vertical=Input.GetAxis("VerticalWASD");
+            }
+            
+
+            steering.linear=new Vector3(horizontal, 0,vertical)*  agent.maxAccel;
+            steering.linear=this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxAccel));
         }
-        else{
-            horizontal=Input.GetAxis("HorizontalWASD");
-            vertical=Input.GetAxis("VerticalWASD");
+        else
+        {
+            List<Vector3> walls = new List<Vector3>();
+
+            // TODO potentially remove this. Makes the cellulo flee the walls in a given radius
+            Collider[] rangeCheck = Physics.OverlapSphere(transform.position, wallRange);
+            foreach(Collider c in rangeCheck)
+            {
+                if(c.gameObject.tag == "Wall")
+                {
+                    walls.Add(c.gameObject.transform.position);
+                }
+            }
+
+            steering.linear = GetMeanVector(walls) * -agent.maxAccel;
+            steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxAccel));
+
         }
         
-
-        steering.linear=new Vector3(horizontal, 0,vertical)*  agent.maxAccel;
-        steering.linear=this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxAccel));
         return steering;
+    }
+
+    // Method to calculate the mean position of a list of vectors
+    private Vector3 GetMeanVector(List<Vector3> positions){
+        if (positions.Count == 0)
+        {
+            return Vector3.zero;
+        }
+        
+        float x = 0f;
+        float z = 0f;
+
+        foreach (Vector3 pos in positions)
+        {
+            x += pos.x;
+            z += pos.z;
+        }
+        return new Vector3(x / positions.Count, 0, z / positions.Count);
     }
 
 }
